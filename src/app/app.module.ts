63b3c570a6e8f3de3ed6114cc6283f72a222c047
Dinh Phu <ndp17hp@gmail.com>
2020-10-27 12:50:39 +0700

    hoan thanh cart icon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeModule } from './home/home.module';
 import { AppRouting } from './app-routing.module';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { HomeRouting } from './home-routing.module';
 import { HomeComponent } from './home.component';
 
+
 @NgModule({
     declarations:[HomeComponent],
     imports:[HomeRouting],
