020fbe5b867220015aea4b1f6ac2760e18999fae
Dinh Phu <ndp17hp@gmail.com>
2020-11-01 14:26:13 +0700

    hoan thanh feature section

--- src/app/shared-component/shared-component.module.ts
+++ src/app/shared-component/shared-component.module.ts
@@ -2,11 +2,21 @@ import { NgModule } from '@angular/core';
 import { HomeCarouselComponent } from './home-carousel/home-carousel.component';
 import { WeeklyFeatureComponent } from './weekly-feature/weekly-feature.component';
 import { BestSellingProductComponent } from './best-selling-product/best-selling-product.component';
+import { FeatureComponent } from './feature/feature.component';
 
 @NgModule({
-    declarations:[HomeCarouselComponent, WeeklyFeatureComponent, BestSellingProductComponent],
+    declarations:[
+        HomeCarouselComponent, 
+        WeeklyFeatureComponent, 
+        BestSellingProductComponent, 
+        FeatureComponent],
     imports:[],
-    exports:[HomeCarouselComponent,WeeklyFeatureComponent,BestSellingProductComponent]
+    exports:[
+        HomeCarouselComponent,
+        WeeklyFeatureComponent,
+        BestSellingProductComponent,
+        FeatureComponent
+    ]
 })
 export class ShareComponent{
 }
