346050704bccfb3e8b837189ac90783be1991135
Dinh Phu <ndp17hp@gmail.com>
2020-11-11 11:58:52 +0700

    bat dau ap dung ngrx cho auth

--- src/app/shared-component/login/login.component.ts
+++ src/app/shared-component/login/login.component.ts
@@ -1,8 +1,9 @@
 import { HttpErrorResponse, HttpResponse } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { NotifierService } from 'angular-notifier';
+import { Subscription } from 'rxjs';
 import { HeaderType } from 'src/app/enum/header-type.enum';
 import { NotificationType } from 'src/app/enum/notification-type.enum';
 import { Role } from 'src/app/enum/role.enum';
@@ -15,10 +16,10 @@ import { NotificationService } from 'src/app/services/notification.service';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
   public loginForm: FormGroup;
-
-
+  public isLoading = false;
+  subscription: Subscription;
   constructor(private authService: AuthenticationService,
     private notifier: NotificationService,
     private router: Router
@@ -30,29 +31,30 @@ export class LoginComponent implements OnInit {
       this.router.navigate(['/admin/action']);
     }
     this.loginForm = new FormGroup({
-      'username': new FormControl(null, Validators.required),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
       'password': new FormControl(null, [Validators.required, Validators.pattern("^[a-zA-Z0-9]{6,}$")])
     })
 
   }
 
-
   onSubmit() {
-
-    this.authService.login(this.loginForm.value).subscribe((resData: HttpResponse<User>) => {
-      console.log(resData);
+    this.isLoading = true;
+    this.subscription = this.authService.login(this.loginForm.value).subscribe((resData: HttpResponse<User>) => {
+      this.isLoading = false;
       const token = resData.headers.get(HeaderType.JWT_TOKEN);
       const user = resData.body;
       console.log(user);
       this.authService.saveToken(token);
       this.authService.addUserToLocalCache(user);
       this.router.navigate(['/admin/action']);
-      
     },
       (errorRes: HttpErrorResponse) => {
         console.log(errorRes);
         this.sendErrorNotification(NotificationType.ERROR, errorRes.error.message);
-      })
+        this.isLoading = false;
+      });
+
+
   }
 
   public sendErrorNotification(type: NotificationType, message: string) {
@@ -60,6 +62,12 @@ export class LoginComponent implements OnInit {
       this.notifier.notify(type, message);
     } else {
       this.notifier.notify(type, 'An error occurred. Please try again!');
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
     }
   }
 }
